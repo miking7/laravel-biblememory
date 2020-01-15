fa5d1a6c3c598c7b53d88a2071ea823f1e9c3545
Michael Engelbrecht <michaelndani@gmail.com>
2020-01-16 09:36:23 +1100

    php artisan make:policy VersePolicy --model=Verse

---
+++ app/Policies/VersePolicy.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Verse;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class VersePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any verses.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the verse.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Verse  $verse
+     * @return mixed
+     */
+    public function view(User $user, Verse $verse)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create verses.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the verse.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Verse  $verse
+     * @return mixed
+     */
+    public function update(User $user, Verse $verse)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the verse.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Verse  $verse
+     * @return mixed
+     */
+    public function delete(User $user, Verse $verse)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the verse.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Verse  $verse
+     * @return mixed
+     */
+    public function restore(User $user, Verse $verse)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the verse.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Verse  $verse
+     * @return mixed
+     */
+    public function forceDelete(User $user, Verse $verse)
+    {
+        //
+    }
+}
